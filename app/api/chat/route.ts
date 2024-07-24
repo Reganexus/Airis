3c8920c1691785330690b903fa3439fe8ba74a5f
Bluchatoe <149781237+Bluchatoe@users.noreply.github.com>
2024-07-24 14:56:26 +0800

    setup

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,5 @@
-import { openai } from '@ai-sdk/openai';
-import { streamText } from 'ai';
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -8,7 +8,8 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = await streamText({
-    model: openai('gpt-4o-mini'),
+    model: openai("gpt-4o-mini"),
+    system: "You are my assistant named bloo.",
     messages,
   });
 
