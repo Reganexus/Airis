67d3545f1db1d0ecc4ed82253bd7ad88c645269b
Bluchatoe <149781237+Bluchatoe@users.noreply.github.com>
2024-07-29 13:14:25 +0800

    moved chat page to its own subdirectory
    Added temporary Persona Selection Page

---
+++ app/chat/page.tsx
@@ -0,0 +1,5 @@
+import { ByteChatBot } from "@/components/component/byte-chat-bot";
+
+export default function ChatPage() {
+  return <ByteChatBot />;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-import { ByteChatBot } from "@/components/component/byte-chat-bot";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <ByteChatBot />
+  return (
+    <main className="flex flex-col justify-center items-center h-screen">
+      <h1 className="m-12 text-5xl">Persona Selection Page</h1>
+      <Link
+        href="/chat"
+        className="bg-primary text-white px-8 py-4 rounded-lg text-2xl"
+      >
+        To Main Chat Page
+      </Link>
+    </main>
+  );
 }
