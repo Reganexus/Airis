d4d256a3c103b563c616ef5f50e5715ded9f623b
lyrick <lyrickjonson@gmail.com>
2024-08-02 14:21:22 +0800

    updated chatbot

--- components/component/byte-chat-bot.tsx
+++ components/component/byte-chat-bot.tsx
@@ -1,16 +1,19 @@
-
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import ToggleButton from "@/components/ui/toggle-button";
+import "@/app/main/persona-selection-scrollbar.css";
 
-import Logo from "@/components/component/logo";
+// import Logo from "@/components/component/logo";
 // import HeaderAvatar from "@/components/component/header-avatar";
+
+import SideBar from "@/app/main/side-bar";
+import PersonaChatHistory from "@/app/chat/persona-chat-history";
 
 import { useChat } from "ai/react";
 import PersonaCard from "./persona-card";
-import { formatTextToHTML } from '@/lib/textToHTML';
+import { formatTextToHTML } from "@/lib/textToHTML";
 import Image from "next/image";
 import SideBar from "./side-bar";
 import { useSession } from "next-auth/react";
@@ -20,8 +23,6 @@ import { generatePreviousChat, generateTitle } from "@/lib/api/gpt-operations";
 import { generateDALLE } from "@/lib/api/dall-e-operations";
 
 import { fetchChatbot, fetchSaveConvo, fetchChatHistory, fetchOldChat, fetchChatUID } from "@/lib/db/fetch-queries";
-
-
 
 interface ByteChatBotProps {
   historyConversationId?: string;
@@ -136,9 +137,9 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
       };
     }
   }, [historyConversationId, status]);
-
   // Default
   // chosenChatbot contains:
+
   /**
    * chatbot_id:
    * created_at:
@@ -160,12 +161,11 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
    */
   const [isLoading2, setIsLoading2] = useState(false);
 
-
   /**
    * Represents the placeholder text for the input field.
    * The placeholder text is displayed when the input field is empty.
    */
-  const [placeholder, setPlaceholder] = useState('Type your message...');
+  const [placeholder, setPlaceholder] = useState("Type your message...");
 
   /**
    * Represents the selected model for generating responses.
@@ -176,6 +176,7 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
   useEffect(() => {
     console.log("QUALITY has been updated to: " + quality);
   }, [quality]); 
+
 
   const [imgSize, setImgSize] = useState('256x256');
   useEffect(() => {
@@ -217,45 +218,44 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
    * @handleInputChange - handles the change event of the input field
    * @handleSubmit      - handles submission event of the chat component (GPT-4o only)
    */
+
   const { messages, setMessages, input, isLoading, handleInputChange, handleSubmit } = useChat();
 
   /**
    * Handles the submission event of both chat components (GPT or DALL-E)
    * this is called by the form
    */
-  async function promptSubmit(e: { preventDefault: () => void; }) {
+  async function promptSubmit(e: { preventDefault: () => void }) {
     e.preventDefault();
-    console.log("MESSAGES: ");
-    console.log(messages);
-    console.log("INPUT: ");
-    console.log(input);
+
     if (model == 'gpt-4o-mini') {
       /**
        * GPT-4o MODEL
        * Calls the app/api/chat/route.ts to stream text output
        */
+
       handleSubmit(e);
     } else {
       /**
        * DALL-E MODEL
        * Calls the app/api/image/route.ts to generate image output
-       * set isLoading2 to true to disable the form while we wait for DALL-E to finish processing 
+       * set isLoading2 to true to disable the form while we wait for DALL-E to finish processing
        * then, manually add the user prompt to messages state, since this is not part of useChat hook
        * fetch DALL-E image url response and manually add it to messages state to display the output on to the UI
        */
       setIsLoading2(true);
       setMessages([
-        ...messages, 
+        ...messages,
         {
           id: "",
-          role: 'user',
-          content: input
+          role: "user",
+          content: input,
         },
         {
           id: "",
-          role: 'assistant',
-          content: ''
-        }
+          role: "assistant",
+          content: "",
+        },
       ]);
 
         generateDALLE(model, input, quality, imgSize, imgStyle, quantity).then((res) => {
@@ -347,25 +347,24 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
     }
       
   }, [status, user]);
-  
+ 
   /**
    * Handles the generation of chat messages based on the provided conversation, message ID, and GPT content.
-   * 
+   *
    * @param convo - The conversation array.
    * @param messageid - The ID of the message to be generated.
    * @param gptcontent - The GPT content to be used for generation.
    */
   function handleGenerate(convo: any[], messageid: number, gptcontent: string) {
-    
     setIsLoading2(true);
     setMessages([
       ...messages.slice(0, messageid),
       {
         id: "",
-        role: 'assistant',
-        content: 'Generating...'
+        role: "assistant",
+        content: "Generating...",
       },
-      ...messages.slice(messageid + 1) 
+      ...messages.slice(messageid + 1),
     ]);
     generatePreviousChat(convo, messageid, gptcontent).then((res) => {
       setIsLoading2(false);
@@ -373,19 +372,18 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
         ...messages.slice(0, messageid),
         {
           id: "",
-          role: 'assistant',
-          content: res.response
+          role: "assistant",
+          content: res.response,
         },
-        ...messages.slice(messageid + 1) 
+        ...messages.slice(messageid + 1),
       ]);
     });
   }
 
-
-
   /**
    * Handles the change of the model and updates the placeholder text when toggle button is clicked.
    */
+
   function handleModelChange(){
     setQuality('standard');
     setImgSize('256x256');
@@ -402,38 +400,38 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
       console.log('MODEL SET TO: ' + model);
       setPlaceholder('Type your message...');
     }
+
+    setIsImageModel((i) => !i);
     //console.log(model);
   }
 
-  /*
-   *
-   */
   const [hoveredMessageIndex, setHoveredMessageIndex] = React.useState<
     number | null
   >(null);
 
+  const [isHistoryOpen, setIsHistoryOpen] = React.useState<boolean>(false);
+
+  const [isImageModel, setIsImageModel] = React.useState<boolean>(false);
+
+  // JSX ELEMENT:
   return (
     <div className="flex h-screen w-full">
+      <SideBar />
+
+      {isHistoryOpen && <PersonaChatHistory />}
+
       {/* Main Content */}
-      <div className="flex flex-1 flex-col">
-        <header className="sticky top-0 z-10 flex justify-between h-14 items-center gap-4 border-b border-slate-200 bg-background px-4 sm:h-[60px] sm:pl-6 sm:pr-3">
-          <Logo />
-
-          <Button variant="ghost" size="icon" className="bg-none">
-            <LeftArrow />
-            <span className="sr-only">More</span>
-          </Button>
-
-          {/* <HeaderAvatar /> */}
-        </header>
-
-        <main className="relative flex-1 overflow-auto pt-5 bg-slate-100 pb-0">
-          <PersonaCard persona={"AI"} />
+      <div className="flex flex-1 flex-col h-screen">
+        <main className="relative overflow-auto pt-5 bg-slate-100 pb-0 h-full">
+          <PersonaCard
+            persona={chosenPersona.persona}
+            setIsOpenHistory={setIsHistoryOpen}
+          />
 
           <div className="pt-4 px-2 ps-4 pb-8 grid gap-6 max-w-5xl m-auto">
             {messages.map((m, i) => {
-              console.log("THIS IS THE SHIT: ");
               console.log(m.content);
+
               const isLastMessage: boolean = i === messages.length - 1;
 
               if (m.role === "user" && m.id != 'firstprompt') {
@@ -460,7 +458,7 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
                     onMouseEnter={() => setHoveredMessageIndex(i)}
                     onMouseLeave={() => setHoveredMessageIndex(null)}
                   >
-                    { /** AI Logo */}
+                    {/** AI Logo */}
                     <div className="pt-4">
                       <div className="h-[40px] w-[40px] rounded-full bg-slate-400 overflow-clip">
                         <Image
@@ -498,7 +496,9 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
                             variant="ghost"
                             size="iconSmall"
                             className="bg-none"
-                            onClick={() => handleGenerate(messages, i, m.content)}
+                            onClick={() =>
+                              handleGenerate(messages, i, m.content)
+                            }
                           >
                             <RegenerateIcon />
                             <span className="sr-only">More</span>
@@ -516,52 +516,159 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
           <div className="sticky bottom-0 h-4 bg-gradient-to-b from-transparent to-slate-100"></div>
         </main>
 
+        {/* INPUT */}
         <form
           onSubmit={promptSubmit}
-          className="pb-[20px] bg-slate-100
-        px-5"
+          className="pb-[20px] bg-slate-100 px-5 max-h-96 flex-1"
         >
-          <div className="rounded-lg max-w-5xl m-auto sticky bottom-0 z-10 flex h-14 items-center gap-2 border bg-background px-4 sm:h-[60px] sm:px-3">
-            <Input
-              type="text"
-              placeholder={placeholder}
-              value={input}
-              className="flex-1 bg-transparent p-2 placeholder:text-base"
-              onChange={handleInputChange}
-              disabled={isLoading || isLoading2}
-            />
-
-
-            <Button
-              variant="default"
-              size="icon"
-              className="bg-primary order-last"
-              disabled={isLoading || isLoading2}
+          <div
+            className={
+              !isImageModel
+                ? `rounded-lg max-w-5xl m-auto z-10 flex items-start gap-2 border bg-white px-4 h-auto sm:px-3 p-2`
+                : `rounded-lg max-w-5xl m-auto z-10 flex gap-2 border bg-white px-4 h-auto sm:px-3 p-2`
+            }
+          >
+            <div
+              className={
+                !isImageModel
+                  ? `w-full flex items-start justify-between`
+                  : `w-full flex flex-col h-auto gap-1 border rounded-md p-2`
+              }
             >
-              <SendIcon />
-              <span className="sr-only">Send</span>
-            </Button>
+              {/* Text Prompt input */}
+              <Input
+                placeholder={placeholder}
+                value={input}
+                className={
+                  !isImageModel
+                    ? `bg-transparent placeholder:text-base p-2 px-4 persona-selection-scrollbar h-auto` //text mode
+                    : `bg-transparent px-1 pt-1 placeholder:text-base h-full persona-selection-scrollbar` //image mode
+                }
+                onChange={handleInputChange}
+                disabled={isLoading || isLoading2}
+              />
 
-            <Button
-              variant="ghost"
-              size="icon"
-              className="bg-none "
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-            >
-              <PlusIcon />
-              <span className="sr-only">More</span>
-            </Button>
+              {/* Hide these buttons if Image mode */}
+              {!isImageModel && (
+                <>
+                  <Button
+                    variant="default"
+                    size="icon"
+                    className="bg-primary order-last p-3"
+                    disabled={isLoading || isLoading2}
+                  >
+                    <SendIcon />
+                    <span className="sr-only">Send</span>
+                  </Button>
 
-            <ToggleButton
-              iconA={<TextIcon />}
-              iconB={<ImageIcon />}
-              className="order-first"
-              changeModel={handleModelChange}
-            />
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="bg-none p-2 mx-2"
+                    onClick={(e) => {
+                      e.preventDefault();
+                    }}
+                  >
+                    <PlusIcon />
+                    <span className="sr-only">More</span>
+                  </Button>
+                </>
+              )}
 
-            <select
+              {/* Toggle between text and image generation */}
+              <ToggleButton
+                iconA={<TextIcon />}
+                iconB={<ImageIcon />}
+                className="order-first self-start"
+                changeModel={handleModelChange}
+              />
+
+              {/* Suggestion Chips */}
+              {isImageModel && <SuggestionChips />}
+            </div>
+
+            {/* Image model setting */}
+            {isImageModel && (
+              <div className="min-w-80 border rounded-lg h-auto flex flex-col p-3 gap-2">
+                <div className="flex justify-between items-center">
+                  <label className="font-semibold text-slate-700" htmlFor="">
+                    Model
+                  </label>
+
+                  <select
+                    name=""
+                    id=""
+                    className="text-sm text-slate-600 py-1 px-2 border rounded-md bg-slate-50"
+                  >
+                    <option value="">DALL-E 3</option>
+                  </select>
+                </div>
+
+                <div className="flex justify-between items-center">
+                  <label className="font-semibold text-slate-700" htmlFor="">
+                    Quality{" "}
+                    <sup className="font-normal text-slate-600">DALL-E3</sup>
+                  </label>
+
+                  <select
+                    name=""
+                    id=""
+                    className="text-sm text-slate-600 py-1 px-2 border rounded-md bg-slate-50"
+                  >
+                    <option value="">Standard</option>
+                  </select>
+                </div>
+
+                <div className="flex justify-between items-center">
+                  <label className="font-semibold text-slate-700" htmlFor="">
+                    Size
+                  </label>
+
+                  <select
+                    name=""
+                    id=""
+                    className="text-sm text-slate-600 py-1 px-2 border rounded-md bg-slate-50"
+                  >
+                    <option value="">1024x1024</option>
+                  </select>
+                </div>
+
+                <div className="flex justify-between items-center">
+                  <label className="font-semibold text-slate-700" htmlFor="">
+                    Style{" "}
+                    <sup className="font-normal text-slate-600">DALL-E3</sup>
+                  </label>
+
+                  <select
+                    name=""
+                    id=""
+                    className="text-sm text-slate-600 py-1 px-2 border rounded-md bg-slate-50"
+                  >
+                    <option value="">Natural</option>
+                  </select>
+                </div>
+
+                <div className="flex justify-between items-center">
+                  <label className="font-semibold text-slate-700" htmlFor="">
+                    Quantity{" "}
+                    <sup className="font-normal text-slate-600">DALL-E2</sup>
+                  </label>
+
+                  <select
+                    name=""
+                    id=""
+                    className="text-sm text-slate-600 py-1 px-2 border rounded-md bg-slate-50"
+                  >
+                    <option value="">1</option>
+                  </select>
+                </div>
+
+                <button className="py-2 bg-primary text-white rounded-lg mt-2">
+                  Generate
+                </button>
+              </div>
+            )}
+            {/*<select
               value={model}
               onChange={(e) => {
                 setModel(e.target.value); 
@@ -625,8 +732,9 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
                   {i + 1}
                 </option>
               ))}
-            </select>
+            </select>*/}
           </div>
+
           <p className="text-sm text-center pt-3 text-slate-500">
             This AI chatbot is for informational purposes only and should not be
             considered professional advice.
@@ -634,12 +742,49 @@ export function ByteChatBot({ historyConversationId }: ByteChatBotProps) {
         </form>
       </div>
 
-      {/* Sidebar */}
-      <SideBar chatHistory={chatHistory} />
+      {/* Previus Sidebar commented out by ken */}
+      {/* <SideBar onPersonaChange={handlePersonaChange} /> */}
+        {/* <SideBar chatHistory={chatHistory} /> */}
     </div>
   );
 }
 
+const dummySuggestions = [
+  { suggestion: "photo" },
+  { suggestion: "illustration" },
+  { suggestion: "3d render" },
+  { suggestion: "typography" },
+];
+
+const SuggestionChips = ({ suggestions = dummySuggestions }) => {
+  return (
+    <div className="w-full mt-auto flex gap-1">
+      {suggestions.map((s) => (
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+          key={s.suggestion}
+          className="bg-slate-50 border rounded-sm py-1 px-2 text-sm text-slate-500 hover:bg-slate-100 hover:text-slate-600"
+        >
+          {s.suggestion}
+        </button>
+      ))}
+
+      {/* More button? */}
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+        className="bg-slate-50 border rounded-sm py-1 px-2 text-sm text-slate-500 hover:bg-slate-100 hover:text-slate-600"
+      >
+        ...
+      </button>
+    </div>
+  );
+};
+
+// ICONS
 function SendIcon() {
   return (
     <svg
@@ -658,7 +803,6 @@ function SendIcon() {
     </svg>
   );
 }
-
 function PlusIcon() {
   return (
     <svg
@@ -677,7 +821,6 @@ function PlusIcon() {
     </svg>
   );
 }
-
 function CopyIcon() {
   return (
     <svg
@@ -696,7 +839,6 @@ function CopyIcon() {
     </svg>
   );
 }
-
 function RegenerateIcon() {
   return (
     <svg
@@ -715,7 +857,6 @@ function RegenerateIcon() {
     </svg>
   );
 }
-
 function TextIcon() {
   return (
     <svg
