0105aeda6bbf85ee947b455987a788f852f89f26
Bluchatoe <149781237+Bluchatoe@users.noreply.github.com>
2024-08-06 14:38:10 +0800

    feat: upload files part 1

---
+++ components/component/uploaded-files.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+type UploadedFilesProps = {};
+
+const UploadedFiles = ({}: UploadedFilesProps) => {
+  return (
+    <div className="max-w-5xl m-auto w-full bg-white mb-1 py-2 px-4 rounded-lg border border-slate-200 text-slate-700">
+      This is file upload
+    </div>
+  );
+};
+
+export default UploadedFiles;
