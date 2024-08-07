2aac20a652c511f05b17e8e7ef5acdc37a1faea7
Bluchatoe <149781237+Bluchatoe@users.noreply.github.com>
2024-08-07 10:41:05 +0800

    fix: hydration issues in logo component

--- app/persona/layout.tsx
+++ app/persona/layout.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { fetchPersonas } from "@/lib/db/fetch-queries";
 import { Persona } from "@/lib/types";
 import { useParams } from "next/navigation";
@@ -12,74 +12,68 @@ export default function SelectionLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // contains the dynamic persona url parameter
+  const { agent } = useParams();
 
-    // contains the dynamic persona url parameter
-    const { agent } = useParams(); 
-    
+  /**
+   * @personaLists - contains all the information returned by the Database, to be updated when the image url is placed
+   * @personaBlobs - contains all the blobs (images) fetched from the AirisBlobStorage
+   * @temp         - temporary placeholder for all  the information of personas
+   */
+  const [personaLists, setPersonaLists] = useState<Persona[]>();
+  const [personaBlobs, setPersonaBlobs] = useState<any[]>([]);
+  const [temp, setTemp] = useState<Persona[]>([]);
+
+  useEffect(() => {
     /**
-     * @personaLists - contains all the information returned by the Database, to be updated when the image url is placed
-     * @personaBlobs - contains all the blobs (images) fetched from the AirisBlobStorage
-     * @temp         - temporary placeholder for all  the information of personas
+     * Get all the personaData [1] and the persona blobs(images) [2] from the database
+     * and store [1] on the temporary variable and [2] on personaBlobs
      */
-    const [personaLists, setPersonaLists] = useState<Persona[]>();
-    const [personaBlobs, setPersonaBlobs] = useState<any[]>([]);
-    const [temp, setTemp] = useState<Persona[]>([]);
+    const fetchPersonaData = async () => {
+      // [1]
+      const personas = await fetchPersonas();
+      setTemp(personas);
 
+      const personalogo = await fetch("/api/image/image-persona-logo", {
+        method: "POST",
+        cache: "no-cache",
+      });
+      const response = await personalogo.json();
 
-    useEffect(() => {
-      /**
-       * Get all the personaData [1] and the persona blobs(images) [2] from the database
-       * and store [1] on the temporary variable and [2] on personaBlobs
-       */
-     const fetchPersonaData = async () => {
-       
-        // [1]
-        const personas = await fetchPersonas();
-        setTemp(personas);
-       
-        const personalogo = await fetch('/api/image/image-persona-logo', {
-          method: "POST",
-          cache: 'no-cache'
+      // [2]
+      const blobs = response.blob;
+      setPersonaBlobs(blobs);
+    };
+    fetchPersonaData();
+  }, []);
+
+  useEffect(() => {
+    /**
+     * Will run when persona data are all fetched
+     * Loop through the blobs and remove the directory of the filename, only using the filename w/ extension
+     * And change the value of persona.logo_name into the blob's URL (previously a logo filename) if filenames matched
+     * Finally, put thee temporary Persona data into the official variable personaData
+     */
+    if (personaBlobs && temp) {
+      personaBlobs.forEach((blob: any) => {
+        const filename = blob.pathname.replace("Assets/persona_icons/", "");
+        temp.forEach((persona) => {
+          if (persona.logo_name === filename) {
+            persona.logo_name = blob.url;
+          }
         });
-        const response = await personalogo.json();
-        
-        // [2]
-        const blobs = response.blob;
-        setPersonaBlobs(blobs)
+        setPersonaLists(temp);
+      });
+    }
+  }, [personaBlobs, temp]);
 
-      };
-      fetchPersonaData();
-        
-    }, []);
-    
-    useEffect(() => {
-      /**
-       * Will run when persona data are all fetched
-       * Loop through the blobs and remove the directory of the filename, only using the filename w/ extension
-       * And change the value of persona.logo_name into the blob's URL (previously a logo filename) if filenames matched
-       * Finally, put thee temporary Persona data into the official variable personaData   
-       */
-      if (personaBlobs && temp) {
-        personaBlobs.forEach((blob: any) => {
-          const filename = blob.pathname.replace('Assets/persona_icons/', '');
-            temp.forEach((persona) => {
-              if (persona.logo_name === filename) {
-                persona.logo_name = blob.url;
-              }
-            });
-            setPersonaLists(temp)
-        });
-      }
-    }, [personaBlobs, temp])
-
-    // Clone children and pass the agent prop (persona_url)
-    const childrenWithProps = React.Children.map(children, (child) => {
-        if (React.isValidElement(child)) {
-            return React.cloneElement(child, { agent } as { [key: string]: any });
-        }
-        return child;
-    });
-
+  // Clone children and pass the agent prop (persona_url)
+  const childrenWithProps = React.Children.map(children, (child) => {
+    if (React.isValidElement(child)) {
+      return React.cloneElement(child, { agent } as { [key: string]: any });
+    }
+    return child;
+  });
 
   return (
     <main className="flex h-screen">
