03d558321510bf5cc8d516806adcd24409d40267
Bluchatoe <149781237+Bluchatoe@users.noreply.github.com>
2024-08-09 13:27:15 +0800

    Update persona-prompts.tsx

--- app/main/persona-prompts.tsx
+++ app/main/persona-prompts.tsx
@@ -75,7 +75,7 @@ const PersonaProfile: React.FC<PersonaChatbotsProps> = ({
         </div>
 
         {/* Persona Action Buttons */}
-        <div className="flex items-center ml-auto text-sm hidden">
+        <div className="items-center ml-auto text-sm hidden">
           <PersonaSettingsButton />
 
           <button className="text-slate-500 dark:text-slate-400 py-2 pr-3 pl-2 hover:bg-slate-100 dark:hover:bg-slate-700 dark:hover:text-slate-300 border border-slate-500  border-r-0 flex items-center gap-2">
@@ -215,15 +215,15 @@ const Prompts: React.FC<PersonaChatbotsProps> = ({ selectedPersona }) => {
     console.log("toilet");
   }
 
-  async function addFrequency(name: any, tagline: any, chatbot_id: any, persona_id: any, task: any) {
-    storeSession(
-      name,
-      tagline,
-      chatbot_id,
-      persona_id,
-      task
-    )
-    updateChatbotFrequency(chatbot_id)
+  async function addFrequency(
+    name: any,
+    tagline: any,
+    chatbot_id: any,
+    persona_id: any,
+    task: any
+  ) {
+    storeSession(name, tagline, chatbot_id, persona_id, task);
+    updateChatbotFrequency(chatbot_id);
   }
   return (
     <div className="border rounded-md flex flex-col rounded-t-none grow dark:border-slate-600">
@@ -371,15 +371,15 @@ const PromptCard: React.FC<PromptCardProps> = ({
     }
   }
 
-  async function addFrequency(name: any, tagline: any, chatbot_id: any, persona_id: any, task: any) {
-    storeSession(
-      name,
-      tagline,
-      chatbot_id,
-      persona_id,
-      task
-    )
-    updateChatbotFrequency(chatbot_id)
+  async function addFrequency(
+    name: any,
+    tagline: any,
+    chatbot_id: any,
+    persona_id: any,
+    task: any
+  ) {
+    storeSession(name, tagline, chatbot_id, persona_id, task);
+    updateChatbotFrequency(chatbot_id);
   }
   return (
     <div
